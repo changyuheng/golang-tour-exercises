72abb83ee35ddfd7bf452386647ae95c4cbd5757
Henry Chang <mr.changyuheng@gmail.com>
2017-04-23 13:21:04 -0700

    Add Maps

---
+++ maps.go
@@ -0,0 +1,20 @@
+// +build ignore
+
+package main
+
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	m := make(map[string]int)
+	for _, v := range strings.Fields(s) {
+		m[v]++
+	}
+	return m
+}
+
+func main() {
+	wc.Test(WordCount)
+}
